88f342279bcc1359d0382627266fc760a9457cee
jordan o'neal <joneal6694@gmail.com>
2020-06-13 12:55:59 -0600

    fixed code to pass lint

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,12 +2,11 @@
 
 // Dependencies
 
-var path = require("path");
+const path = require("path");
 
 // Routes
 
 module.exports = function(app) {
-
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
@@ -29,5 +28,4 @@ module.exports = function(app) {
   app.get("/authors", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/author-manager.html"));
   });
-
 };
