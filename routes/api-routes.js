0b8d6b618302512dfb9e7ed3165afa7735683626
Florencia Tabarez <fftabarez97@gmail.com>
2020-06-13 13:35:17 -0600

    Update.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,49 +1,48 @@
-var db = require("../models");
+const db = require("../models");
 
-module.exports = function(app) {
-
-    app.get("/api/posts/:zip", function(req, res) {
-        db.Post.findAll({
-            where: {
-                zip: req.params.zip
-            }
-        }).then(function(dbPost) {
-            res.json(dbPost);
-        });
+module.exports = function (app) {
+  app.get("/api/posts/:zip", (req, res) => {
+    db.Post.findAll({
+      where: {
+        zip: req.params.zip
+      }
+    }).then(dbPost => {
+      res.json(dbPost);
     });
+  });
 
-    app.get("/api/posts/:city/:state/:zip", function(req, res) {
-        db.Post.findAll({
-            where: {
-                city: req.params.city,
-                state: req.params.state,
-                zip: req.params.zip
-            }
-        }).then(function(dbPost) {
-            res.json(dbPost);
-        });
+  app.get("/api/posts/:city/:state/:zip", (req, res) => {
+    db.Post.findAll({
+      where: {
+        city: req.params.city,
+        state: req.params.state,
+        zip: req.params.zip
+      }
+    }).then(dbPost => {
+      res.json(dbPost);
     });
+  });
 
-    app.post("/api/posts/:zip", function(req, res) {
-        console.log(req.body);
-        db.Post.create({
-            author: req.body.author,
-            title: req.body.title,
-            text: req.body.text
-            // comments?
-        }).then(function(dbPost) {
-            res.json(dbPost);
-        });
+  app.post("/api/posts/:zip", (req, res) => {
+    console.log(req.body);
+    db.Post.create({
+      author: req.body.author,
+      title: req.body.title,
+      text: req.body.text
+      // comments?
+    }).then(dbPost => {
+      res.json(dbPost);
     });
+  });
 
-    app.post("/api/posts/:city/:state/:zip", function(req, res) {
-        db.Post.create({
-            author: req.body.author,
-            title: req.body.title,
-            text: req.body.text
-            // comments?
-        }).then(function(dbPost) {
-            res.json(dbPost);
-        });
+  app.post("/api/posts/:city/:state/:zip", (req, res) => {
+    db.Post.create({
+      author: req.body.author,
+      title: req.body.title,
+      text: req.body.text
+      // comments?
+    }).then(dbPost => {
+      res.json(dbPost);
     });
-}
+  });
+};
