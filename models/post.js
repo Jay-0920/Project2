b253379f00fdd12eea1958448d6681af42235ac2
Florencia Tabarez <fftabarez97@gmail.com>
2020-06-13 10:21:44 -0600

    Updated Models with correct associations for post.js and comment.js.

--- models/post.js
+++ models/post.js
@@ -15,17 +15,14 @@ module.exports = function(sequelize, DataTypes) {
     body: {
       type: DataTypes.TEXT,
       allowNull: false,
-      len: [250]
+      len: [1]
     }
   });
 
   Post.associate = function(models) {
-    // We're saying that a Post should belong to an Author
-    // A Post can't be created without an Author due to the foreign key constraint
-    Post.belongsTo(models.Post, {
-      foreignKey: {
-        allowNull: false
-      }
+    
+    Post.hasMany(models.Comment, {
+      onDelete: "cascade"
     });
   };
 
