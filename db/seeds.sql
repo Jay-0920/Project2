fc97998c23b167a7bffd05befcc15b6e7154b570
Florencia Tabarez <fftabarez97@gmail.com>
2020-06-16 20:09:24 -0600

    Deleted unused API Route, created seeds.sql.

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+USE wepin_db;
+
+INSERT INTO post(author, title, body, location)
+VALUES ("anonymous", "George Floyd Killed by Police" , "The private autopsy by doctors hired by Mr. Floyd’s family determined that he died not just because of the knee on his neck — held there by the officer, Derek Chauvin — but also because of two other officers who helped pin him down by applying pressure on his back. All three officers were fired last week, as was a fourth officer at the scene.
+
+The cause of death, according to the private autopsy, was mechanical asphyxia and the manner of death was homicide.
+
+https://www.nytimes.com/article/george-floyd-autopsy-michael-baden.html", "55413");
+
+INSERT INTO post(author, title, body, location)
+VALUES ("#Justice4BreonnaTaylor", "Breonna Taylor Killed by Police in No-Knock Raid", "'I don't know what happened ... somebody kicked in the door and shot my girlfriend,' Walker told the dispatcher. When asked where Taylor had been shot, Walker replied, 'I don't know, she is on the ground right now. I don't know, I don't know.'  
+
+He added that Taylor was unresponsive, yelling 'Help!' and 'Oh my God,' throughout the two-minute call. The audio of the call was released by an attorney for Taylor's family, CBS affiliate WLKY-TV reported.
+
+https://www.cbsnews.com/news/breonna-taylor-kenneth-walker-911-call-police-shooting/", "40202");
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,18 +12,9 @@ module.exports = function(app) {
     });
   });
 
-<<<<<<< HEAD
-  // GET route for getting all posts by city, state, and zip code
-  app.get("/api/post/:city/:state/:zip", function(req, res) {
-    db.Post.findAll({
-      where: {
-        city: req.params.city,
-        state: req.params.state,
-=======
   // GET route for getting all of the posts
   app.get("/api/post/", function(req, res) {
     db.Post.findAll({}).then(function(dbPost) {
->>>>>>> 5986f8a5b33c17afc20b6734c11c43930c7f754b
       res.json(dbPost);
     });
 
@@ -60,25 +51,8 @@ module.exports = function(app) {
       author: req.body.author,
       title: req.body.title,
       body: req.body.body
-<<<<<<< HEAD
-      // comments?
-    }).then(dbPost => {
-=======
     }).then(function(dbPost) {
->>>>>>> 5986f8a5b33c17afc20b6734c11c43930c7f754b
       res.json(dbPost);
     });
   });
-
-  // // POST route for saving a new post
-  // app.post("/api/posts/:city/:state/:zip", function(req, res) {
-  //   db.Post.create({
-  //     author: req.body.author,
-  //     title: req.body.title,
-  //     body: req.body.body
-  //     // comments?
-  //   }).then(function(dbPost) {
-  //     res.json(dbPost);
-  //   });
-  // });
-};
+}
